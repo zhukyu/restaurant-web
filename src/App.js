1fed56bcaace52672c7740a92c08931f65e4aaa2
Tan Dung <tandung12072002@gmail.com>
2022-09-30 12:19:45 +0700

    route

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
             }
           />
           <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
         </Routes>
       </div>
     </Router>

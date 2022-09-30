9419655f97315c83c3d61b4d8ca0e8ebaa6557f1
zhukyu <phamtienhuu562002@gmail.com>
2022-09-30 11:59:51 +0700

    login and register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Register.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Register() {
+    const navigate = useNavigate();
+    const registerSubmit = () => {
+        navigate('/')
+    }
+    return (
+        <div className='Login' style={{width: "100vw", height: "100vh"}}>
+            <div className="container py-5">
+                <div className="row justify-content-center d-flex">
+                    <div className='col-md-6'>
+                        <div className='card'>
+                            <div className="card-header">
+                                <h4 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate'>Register
+                                    <Link to={'/'} className="btn btn-primary justify-center py-2 px-4 border float-end">Back</Link>
+                                </h4>
+                            </div>
+                            <div className='card-body'>
+                                <form className='form-register' onSubmit={registerSubmit}>
+                                    <div className="grid grid-cols-2 gap-6">
+                                        <div className="col-span-3 sm:col-span-2">
+                                            <label className="block text-sm font-medium text-gray-700">
+                                                Email
+                                            </label>
+                                            <div className="my-1 flex rounded-md shadow-sm">
+                                                <input
+                                                    name="email"
+                                                    type="email"
+                                                    className="form-control focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                                                />
+                                            </div>
+                                                <span className='text-sm font-medium text-danger'>{}</span>
+                                            <label className="block text-sm font-medium text-gray-700">
+                                                Password
+                                            </label>
+                                            <div className="mt-1 flex rounded-md shadow-sm">
+                                                <input
+                                                    name="password"
+                                                    type="password"
+                                                    className="form-control focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                                                />
+                                            </div>
+                                            <label className="block text-sm font-medium text-gray-700">
+                                                Confirm Password
+                                            </label>
+                                            <div className="mt-1 flex rounded-md shadow-sm">
+                                                <input
+                                                    name="password-confirm"
+                                                    type="password"
+                                                    className="form-control focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                                                />
+                                            </div>
+                                                <span className='text-sm font-medium text-danger'>{}</span>
+                                            <div className='form-group '>
+                                                <button type="submit" className='btn btn-primary justify-center py-2 px-4 border mt-3 float-end'>Register</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
